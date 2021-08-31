8cd928b722a826b45c46bb4cce56f00c6cabc606
leome <leome@192.168.1.39>
2021-08-30 19:36:07 -0500

    Initial Commit Pt. 2

---
+++ src/Hello/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * Author: Leo Merrill
+ * Date: 2021/08/30
+ *
+ *  A simple Name printing program in C
+ *
+ */
+package Hello;
+
+public class Hello {
+
+		public static void main(String args[]) {
+			System.out.println("Leo Merrill!");
+		}
+}
